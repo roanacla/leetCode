1b54abb92c29068f7a905d39cc7ac402f11e08a9
roanacla <roanacla@me.com>
2021-02-02 10:55:51 -0800

    Test algorithm to join two sorted arrays'

--- Sorting and Searching/FindMedianSortedArrays/Sources/FindMedianSortedArrays/FindMedianSortedArrays.swift
+++ Sorting and Searching/FindMedianSortedArrays/Sources/FindMedianSortedArrays/FindMedianSortedArrays.swift
@@ -5,7 +5,10 @@ struct FindMedianSortedArrays {
 
 func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
   if nums1.isEmpty && nums2.isEmpty { return 0 }
-  let merge = (nums1 + nums2).sorted()
+  //Option 1
+  // let merge = (nums1 + nums2).sorted()
+  //Option 2 //
+  let merge = joinInOrder(a: nums1, b: nums2)
   var result: Double = 0
   let half = merge.count / 2
   
@@ -18,3 +21,35 @@ func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
   return result
 }
 
+func joinInOrder (a: [Int], b: [Int]) -> [Int] {
+  var result = [Int]()
+  if a.isEmpty && b.isEmpty {
+    return []
+  } else if a.isEmpty {
+    return b
+  } else if b.isEmpty {
+    return a
+  } else {
+    var aIndex = a.startIndex
+    var bIndex = b.startIndex
+    
+    for _ in 0..<(a.count + b.count) {
+      if aIndex < a.endIndex && bIndex < b.endIndex {
+        let elementA = a[aIndex]
+        let elementB = b[bIndex]
+        let isABigger = elementA >= elementB
+        result.append(isABigger ? elementB : elementA)
+        aIndex = isABigger ? aIndex : a.index(after: aIndex)
+        bIndex = isABigger ? b.index(after: bIndex) : bIndex
+      } else {
+        break
+      }
+    }
+    if aIndex == a.endIndex {
+      result += Array(b[bIndex..<b.endIndex])
+    } else {
+      result += Array(a[aIndex..<a.endIndex])
+    }
+  }
+  return result
+}
--- Sorting and Searching/FindMedianSortedArrays/Tests/FindMedianSortedArraysTests/FindMedianSortedArraysTests.swift
+++ Sorting and Searching/FindMedianSortedArrays/Tests/FindMedianSortedArraysTests/FindMedianSortedArraysTests.swift
@@ -2,31 +2,40 @@ import XCTest
 @testable import FindMedianSortedArrays
 
 final class FindMedianSortedArraysTests: XCTestCase {
-    func testExample() {
-      var nums1 = [1,2,3,4,5,6]//[Int]()//[2]//[Int]()//[0,0]//[1,2]//[1,3]
-      var nums2 = [7]//[Int]()//[Int]()//[1]//[0,0]//[3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 4.0)
-      nums1 = [Int]()//[2]//[Int]()//[0,0]//[1,2]//[1,3]
-      nums2 = [Int]()//[Int]()//[1]//[0,0]//[3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 0.0)
-      nums1 = [2]//[Int]()//[0,0]//[1,2]//[1,3]
-      nums2 = [Int]()//[1]//[0,0]//[3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.0)
-      nums1 = [Int]()//[0,0]//[1,2]//[1,3]
-      nums2 = [1]//[0,0]//[3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 1.0)
-      nums1 = [0,0]//[1,2]//[1,3]
-      nums2 = [0,0]//[3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 0.0)
-      nums1 = [1,2]//[1,3]
-      nums2 = [3,4]//[2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.5)
-      nums1 = [1,3]
-      nums2 = [2]
-      XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.0)
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testExample() {
+    var nums1 = [1,2,3,4,5,6]//[Int]()//[2]//[Int]()//[0,0]//[1,2]//[1,3]
+    var nums2 = [7]//[Int]()//[Int]()//[1]//[0,0]//[3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 4.0)
+    nums1 = [Int]()//[2]//[Int]()//[0,0]//[1,2]//[1,3]
+    nums2 = [Int]()//[Int]()//[1]//[0,0]//[3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 0.0)
+    nums1 = [2]//[Int]()//[0,0]//[1,2]//[1,3]
+    nums2 = [Int]()//[1]//[0,0]//[3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.0)
+    nums1 = [Int]()//[0,0]//[1,2]//[1,3]
+    nums2 = [1]//[0,0]//[3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 1.0)
+    nums1 = [0,0]//[1,2]//[1,3]
+    nums2 = [0,0]//[3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 0.0)
+    nums1 = [1,2]//[1,3]
+    nums2 = [3,4]//[2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.5)
+    nums1 = [1,3]
+    nums2 = [2]
+    XCTAssertEqual(findMedianSortedArrays(nums1, nums2), 2.0)
+  }
+  
+  func testJoinArrays() {
+    let a = [1,2]
+    let b = [3,4]
+    let answer = [1,2,3,4]
+    
+    XCTAssertEqual(answer, joinInOrder(a:a,b:b))
+    
+  }
+  
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
