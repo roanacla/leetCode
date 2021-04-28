d2e3cf455bebb6959de2cced7c1d21d7364540c7
roanacla <roanacla@me.com>
2021-04-28 09:36:35 -0700

    Improve 217. Contains Duplicate

--- Arrays/217. Contains Duplicate/Sources/217. Contains Duplicate/_17__Contains_Duplicate.swift
+++ Arrays/217. Contains Duplicate/Sources/217. Contains Duplicate/_17__Contains_Duplicate.swift
@@ -1,7 +1,15 @@
 
 func containsDuplicate(_ nums: [Int]) -> Bool {
-  guard nums.count > 1 else { return false }
-  let setOfNums = Set(nums)
-  return setOfNums.count != nums.count
+  func containsDuplicate(_ nums: [Int]) -> Bool {
+    var nonDuplicates: Set<Int> = []
+    for num in nums {
+      if !nonDuplicates.contains(num) {
+        nonDuplicates.insert(num)
+      } else {
+        return true
+      }
+    }
+    return false
+  }
 }
 
