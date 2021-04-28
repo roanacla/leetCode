3999ada1c5903c42e44dc0b74da4e06ae644861b
roanacla <roanacla@me.com>
2021-04-28 11:49:14 -0700

    Improve 448. Find all Numbers disappeared in an Array

--- Arrays/448. Find All Numbers Disappeared in an Array/Sources/448. Find All Numbers Disappeared in an Array/_48__Find_All_Numbers_Disappeared_in_an_Array.swift
+++ Arrays/448. Find All Numbers Disappeared in an Array/Sources/448. Find All Numbers Disappeared in an Array/_48__Find_All_Numbers_Disappeared_in_an_Array.swift
@@ -1,18 +1,38 @@
-//O(n) with extra space complexity of O(2n)
-func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
-  guard !nums.isEmpty else { return nums }
-  var setNums = Set(nums)
-  var result: [Int] = []
+//Time complexity O(2n) -> two passes
+//Space complexity O(1) -> inplace detection
 
-  for num in 1...nums.count {
-    if !setNums.contains(num) {
-      result.append(num)
+func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+  var nums = nums
+  var result: [Int] = []
+  for i in 0..<nums.count {
+    let num = abs(nums[i])
+    if nums[num-1] > 0 {
+      nums[num-1] = nums[num-1] * -1
     }
   }
-
+  for i in 0..<nums.count {
+    if nums[i] > 0 {
+      result.append(i+1)
+    }
+  }
   return result
-
 }
+
+//O(n) with extra space complexity of O(2n)
+//func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+//  guard !nums.isEmpty else { return nums }
+//  var setNums = Set(nums)
+//  var result: [Int] = []
+//
+//  for num in 1...nums.count {
+//    if !setNums.contains(num) {
+//      result.append(num)
+//    }
+//  }
+//
+//  return result
+//
+//}
 
 //O(n)
 //func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
