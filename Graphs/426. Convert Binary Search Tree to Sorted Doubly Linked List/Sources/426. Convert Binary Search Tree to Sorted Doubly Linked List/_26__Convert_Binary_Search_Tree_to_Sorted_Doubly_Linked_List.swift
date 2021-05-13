6055e0193ea81e13f262717f8d0ebe1880b227b5
roanacla <roanacla@me.com>
2021-05-12 17:20:18 -0700

    426. Convert Binary Search Tree to Sorted Doubly Linked List

--- Graphs/426. Convert Binary Search Tree to Sorted Doubly Linked List/Sources/426. Convert Binary Search Tree to Sorted Doubly Linked List/_26__Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.swift
+++ Graphs/426. Convert Binary Search Tree to Sorted Doubly Linked List/Sources/426. Convert Binary Search Tree to Sorted Doubly Linked List/_26__Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.swift
@@ -1,3 +1,40 @@
 struct _26__Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List {
-    var text = "Hello, World!"
+  var text = "Hello, World!"
+}
+
+public class Node {
+  public var val: Int
+  public var left: Node?
+  public var right: Node?
+  public init(_ val: Int) {
+    self.val = val
+    self.left = nil
+    self.right = nil
+  }
+}
+
+class Solution {
+  var first: Node? = nil
+  var last: Node? = nil
+  
+  func treeToDoublyList(_ root: Node?) -> Node? {
+    helper(root)
+    last?.right = first
+    first?.left = last
+    return first
+  }
+  
+  func helper(_ node: Node?) {
+    guard let node = node else { return }
+    
+    helper(node.left)
+    if let last = last {
+      last.right = node
+      node.left = last
+    } else {
+      first = node
+    }
+    last = node
+    helper(node.right)
+  }
 }
