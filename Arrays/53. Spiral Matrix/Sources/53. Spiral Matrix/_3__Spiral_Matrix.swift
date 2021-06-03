e6ec2572d4efbf351e0792c21b8bff2ebe770227
roanacla <roanacla@me.com>
2021-06-02 18:28:02 -0700

    Improve 53. Spiral Matrix

--- Arrays/53. Spiral Matrix/Sources/53. Spiral Matrix/_3__Spiral_Matrix.swift
+++ Arrays/53. Spiral Matrix/Sources/53. Spiral Matrix/_3__Spiral_Matrix.swift
@@ -1,58 +1,33 @@
-enum MatrixSide {
-  case Top
-  case Bottom
-  case Left
-  case Right
-}
-
-func spiralOrder(_ matrix: [[Int]]) -> [Int] {
-  
-  var hRange = 0..<matrix[0].count // Horizontal range | it shrinks on every after reaching to bottom of the matrix
-  var result: [Int] = []
-  
-  func getValuesFrom(_ row: Int, _ side: MatrixSide) -> [Int] {
-    switch side {
-    case .Top:
-      return Array(matrix[row][hRange]) //return the entire row inside range
-    case .Bottom:
-      return matrix[row][hRange].reversed() // return the entire row (reversed) inside the range
-    case .Right:
-      return [matrix[row][hRange.upperBound - 1]] // only the last element of the range
-    case .Left:
-      return [matrix[row][hRange.lowerBound]] // only the first element range
+class Solution {
+  func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+    guard  !matrix.isEmpty else {
+      return []
     }
-  }
-  
-  var row = 0; var topEdge = 0; var bottomEdge = matrix.count - 1
-  var increase = true
-  while row <= bottomEdge {
-    if bottomEdge < topEdge { break }
-    if row == 0 { result += getValuesFrom(row, .Top); row += 1; continue } //
-    if increase {
-      if row == bottomEdge {
-        result += getValuesFrom(row, .Bottom)
-        increase = false; bottomEdge -= 1; topEdge += 1; row -= 1
-        if hRange.lowerBound < hRange.upperBound - 1 {
-          hRange = (hRange.lowerBound)..<(hRange.upperBound - 1)
-        } else { break }
-        continue
+    var top = 0
+    var bottom = matrix.count - 1
+    var left = 0
+    var right = matrix[0].count - 1
+    let count = matrix.count * matrix[0].count
+    var arr = [Int]()
+    while arr.count < count {
+      
+      for i in stride(from: left, to: right+1, by: 1) where arr.count < count {
+        arr.append(matrix[top][i])
       }
-      result += getValuesFrom(row, .Right)
-      row += 1
-    } else {
-      if row == topEdge {
-        result += getValuesFrom(row, .Top)
-        increase = true; row += 1
-        if hRange.lowerBound < hRange.upperBound - 1 {
-          hRange = (hRange.lowerBound + 1)..<(hRange.upperBound)
-        } else { break }
-        continue
+      top += 1
+      for i in stride(from: top, to: bottom+1, by: 1) where arr.count < count {
+        arr.append(matrix[i][right])
       }
-      result += getValuesFrom(row, .Left)
-      row -= 1
+      right -= 1
+      for i in stride(from: right, to: left-1, by: -1) where arr.count < count {
+        arr.append(matrix[bottom][i])
+      }
+      bottom -= 1
+      for i in stride(from: bottom, to: top-1, by: -1) where arr.count < count {
+        arr.append(matrix[i][left])
+      }
+      left += 1
     }
+    return arr
   }
-  
-  
-  return result
 }
