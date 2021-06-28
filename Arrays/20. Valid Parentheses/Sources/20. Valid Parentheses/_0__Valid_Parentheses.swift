aaa53779db2aa48bafae59ad7fffed8b0553fcfc
roanacla <>
2021-06-28 15:20:35 -0700

    20. Valid Parentheses

--- Arrays/20. Valid Parentheses/Sources/20. Valid Parentheses/_0__Valid_Parentheses.swift
+++ Arrays/20. Valid Parentheses/Sources/20. Valid Parentheses/_0__Valid_Parentheses.swift
@@ -1,42 +1,25 @@
 struct _0__Valid_Parentheses {
-    var text = "Hello, World!"
+  var text = "Hello, World!"
 }
 
 class Solution {
-    func isValid(_ s: String) -> Bool {
-        typealias Result = (parenthesis: Int,
-                          curlyBrackets: Int,
-                          squareBrackets: Int)
-        
-        var result: Result = (0,0,0)
-        var stack: [Character] = []
-
-        for char in s {
-            
-            switch char {
-                case "(":
-                    result.parenthesis += 1
-                    stack.append(char)
-                case ")":
-                    result.parenthesis -= 1
-                    guard let symbol = stack.popLast(), symbol == "(" else { return false }
-                case "{":
-                    result.curlyBrackets += 1
-                    stack.append(char)
-                case "}":
-                    result.curlyBrackets -= 1
-                    guard let symbol = stack.popLast(), symbol == "{" else { return false }
-                case "[":
-                    result.squareBrackets += 1
-                    stack.append(char)
-                case "]":
-                    result.squareBrackets -= 1
-                    guard let symbol = stack.popLast(), symbol == "[" else { return false }
-                default:
-                    return false
-            }
+  func isValid(_ s: String) -> Bool {
+    var openCharacters: [Character] = []
+    
+    for character in s {
+      if character == "(" || character == "[" || character == "{" {
+        openCharacters.append(character)
+      } else {
+        guard !openCharacters.isEmpty else { return false }
+        let lastOpened = openCharacters.popLast()
+        if lastOpened == "(" && character != ")" ||
+            lastOpened == "[" && character != "]" ||
+            lastOpened == "{" && character != "}" {
+          return false
         }
-        
-        return result == (0,0,0) ? true : false
+      }
     }
+    
+    return openCharacters.isEmpty
+  }
 }
