2cf6ae1add0e361a5841540bad980093e646cd9f
Roger Navarro <roanacla@me.com>
2025-12-03 16:22:53 -0800

    153. Updated

--- [25]leetcode.playground/Sources/Binary Search/153. Find Minimun In Rotated Sorted Array.swift
+++ [25]leetcode.playground/Sources/Binary Search/153. Find Minimun In Rotated Sorted Array.swift
@@ -1,20 +1,25 @@
 class Problem_153 {
     func findMin(_ nums: [Int]) -> Int {
-        var mid = nums.count / 2
         var leftEdge = 0
         var rightEdge = nums.count - 1
 
-        while leftEdge <= rightEdge {
-            if nums[leftEdge] <= nums[rightEdge] {
-                return nums[leftEdge]
-            } else if nums[mid] < nums[rightEdge] {
-                rightEdge = mid
-            } else if nums[mid] > nums[rightEdge] {
+        // When loop ends, leftEdge == rightEdge, and that index holds the minimum.
+        while leftEdge < rightEdge {
+            let mid = leftEdge + (rightEdge - leftEdge) / 2
+
+            // If mid is greater than the right end, the pivot must be on the right.
+            if nums[mid] > nums[rightEdge] {
                 leftEdge = mid + 1
             }
-            mid = leftEdge + ((rightEdge - leftEdge) / 2)
+            // If mid is less than or equal to the right end, the right side is sorted,
+            // and the minimum is either mid or on the left. Set rightEdge = mid
+            // to keep mid in the search space.
+            else {
+                rightEdge = mid
+            }
         }
 
-        return nums[mid]
+        // When leftEdge == rightEdge, we found the minimum.
+        return nums[leftEdge]
     }
 }
