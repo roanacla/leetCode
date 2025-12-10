e0b9056f48348038f575079b90b7b0f230eaeb1c
Roger Navarro <roanacla@me.com>
2025-12-09 16:55:34 -0800

    977. Squares of a sorted array v2

--- [25]leetcode.playground/Sources/Two pointers/977. Squares of a softed array.swift
+++ [25]leetcode.playground/Sources/Two pointers/977. Squares of a softed array.swift
@@ -23,6 +23,26 @@ public class Problem_977: Runner {
         return result
     }
     
+    func sortedSquaresTwo(_ nums: [Int]) -> [Int] {
+        var leftEdge = 0
+        var rightEdge = nums.count - 1
+        var index = rightEdge
+        var result: [Int] = Array(repeating: 0, count: nums.count)
+        
+        while 0 <= index {
+            if abs(nums[leftEdge]) < abs(nums[rightEdge]) {
+                result[index] = nums[rightEdge] * nums[rightEdge]
+                rightEdge -= 1
+            } else {
+                result[index] = nums[leftEdge] * nums[leftEdge]
+                leftEdge += 1
+            }
+            index -= 1
+        }
+        
+        return result
+    }
+    
     public func runCode() {
         print("977. Squares of a Sorted Array")
         let nums = [-4, -1, 0, 3, 10]
